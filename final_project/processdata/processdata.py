0f35819b7ee2c8548a693e91fa1fc1ad97aa1226
Ted McIntosh <tem614@g.harvard.edu>
2020-12-06 16:46:37 -0500

    Added consensing of state population data

--- final_project/processdata/processdata.py
+++ final_project/processdata/processdata.py
@@ -1,10 +1,11 @@
 import os
 
 import geopandas as gpd
+from csci_utils.luigi.dask import ParquetTarget
 from csci_utils.luigi.task import Requirement, Requires, TargetOutput
 from luigi import LocalTarget, Task
 
-from ..getdata import ShapeFiles
+from ..getdata import DailyCovidData, ShapeFiles, StatePopulation
 
 
 class CondensedShapefile(Task):
@@ -22,5 +23,81 @@ class CondensedShapefile(Task):
         gdf = gpd.read_file(self.input()["shapefile"].path)[
             ["STATEFP", "NAME", "geometry"]
         ]
-        gdf = gdf[~gdf["NAME"].isin(["Hawaii", "Alaska", "Puerto Rico"])]
+        gdf = gdf[gdf["STATEFP"].isin(contiguous_states)]
         gdf.to_file(self.output().path)
+
+
+class CondensedStatePop(Task):
+
+    requires = Requires()
+    populations = Requirement(StatePopulation)
+    output = TargetOutput(
+        file_pattern=os.path.join("data", "{task.__class__.__name__}/"),
+        ext="",
+        target_class=ParquetTarget,
+        glob="*.parquet",
+    )
+
+    def run(self):
+        ddf = self.input()["populations"].read_dask(
+            usecols=["STATE", "NAME", "POPESTIMATE2019"]
+        )
+        ddf = ddf[ddf["STATE"].isin(contiguous_states)]
+        self.output().write_dask(ddf, compression="gzip")
+
+
+class CleanedCovidData(Task):
+    requires = Requires()
+    populations = Requirement(DailyCovidData)
+
+
+contiguous_states = [
+    1,
+    4,
+    5,
+    6,
+    8,
+    9,
+    10,
+    12,
+    13,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+    32,
+    33,
+    34,
+    35,
+    36,
+    37,
+    38,
+    39,
+    40,
+    41,
+    42,
+    44,
+    45,
+    46,
+    47,
+    48,
+    49,
+    50,
+    51,
+    53,
+    54,
+    55,
+    56,
+]
