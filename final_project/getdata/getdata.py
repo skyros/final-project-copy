a0a05e0f1665d808a2680223f50c6157647ae1cb
Ted McIntosh <tem614@g.harvard.edu>
2020-12-14 18:05:36 -0500

    cleaning up get data tasks

--- final_project/getdata/getdata.py
+++ final_project/getdata/getdata.py
@@ -7,8 +7,6 @@ from luigi import ExternalTask, LocalTarget, Task
 
 from ..utils import LocalShapeFileTarget
 
-covid_path = os.path.join("data", "DailyCovidData")
-
 
 class DailyCovidData(Task):
     """
@@ -19,9 +17,10 @@ class DailyCovidData(Task):
     """
 
     API_Path = "https://api.covidtracking.com/v1/states/daily.csv"
+    SHARED_DIRECTORY = os.path.join("data", "DailyCovidData")
 
     output = TargetOutput(
-        file_pattern=os.path.join(covid_path, "0"),
+        file_pattern=os.path.join(SHARED_DIRECTORY, "0"),
         ext=".csv",
         target_class=LocalTarget,
     )
@@ -35,11 +34,12 @@ class DailyCovidData(Task):
 class DaskFSDailyCovidData(Task):
     """A little bit of a hack - Task that reclassifies the existing path as a dask CSVTarget"""
 
+    SHARED_DIRECTORY = os.path.join("data", "DailyCovidData")
     requires = Requires()
     covid_data = Requirement(DailyCovidData)
 
     output = TargetOutput(
-        file_pattern=covid_path + "/",
+        file_pattern=SHARED_DIRECTORY + "/",
         ext="",
         target_class=CSVTarget,
         flag=None,
